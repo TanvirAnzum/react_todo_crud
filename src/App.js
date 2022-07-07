e21458b6e44defa7530eef61e674db9a12619a95
unknown <tanviranzum14@gmail.com>
2022-07-07 21:53:42 +0600

    added

--- src/App.js
+++ src/App.js
@@ -1,23 +1,91 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [todoTitle, setTodoTitle] = useState("");
+  const [todoList, setTodoList] = useState([]);
+  const [isEditable, setIsEditable] = useState(false);
+  const [editableTodo, setEditableTodo] = useState(null);
+
+  const newTodo = {
+    id: Date.now(),
+    title: todoTitle,
+  };
+
+  const getInput = (event) => {
+    setTodoTitle(event.target.value);
+  };
+
+  const createList = () => {
+    setTodoList([newTodo, ...todoList]);
+    setTodoTitle("");
+  };
+
+  const editList = (id) => {
+    let toBeEdited = todoList.find((todo) => todo.id === id);
+    setTodoTitle(toBeEdited.title);
+    setIsEditable(true);
+    setEditableTodo(toBeEdited);
+    setTodoTitle(toBeEdited.title);
+  };
+
+  const updateList = () => {
+    editableTodo.title = todoTitle;
+    setTodoTitle("");
+    setIsEditable(false);
+    setEditableTodo(null);
+  };
+
+  const deleteList = (id) => {
+    let filteredList = todoList.filter((todo) => todo.id !== id);
+    setTodoList(filteredList);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+      <h1>TODO List with CRUD</h1>
+      <form action="" className="Form">
+        <input
+          type="text"
+          name="input"
+          id="input"
+          value={todoTitle}
+          placeholder="New Task"
+          onChange={(event) => {
+            getInput(event);
+          }}
+        />
+        <button
+          className="submitBtn"
+          onClick={(e) => {
+            e.preventDefault();
+            isEditable ? updateList() : createList();
+          }}
         >
-          Learn React
-        </a>
-      </header>
+          {isEditable ? "Edit Task" : "Create Task"}
+        </button>
+      </form>
+      <ul>
+        {todoList.map((todo) => (
+          <li>
+            <p>{todo.title}</p>
+            <button
+              onClick={() => {
+                editList(todo.id);
+              }}
+            >
+              Edit
+            </button>
+            <button
+              onClick={() => {
+                deleteList(todo.id);
+              }}
+            >
+              Delete
+            </button>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
